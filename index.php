5b74a6e2be75badcec74f6fc96c29bfc33623873
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-18 14:28:22 +0100

    replace-list-with-table

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
-
+// Include
 include 'includes/data/hotels.php';
 
 ?>
 
+<!-- Html -->
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
     <title>PHP Hotel</title>
 
     <!-- Bootstrap -->
@@ -17,20 +19,42 @@ include 'includes/data/hotels.php';
 </head>
 
 <body>
+    <!-- Main -->
     <main>
-        <section id="hotels">
-            <h1>Hotels </h1>
-            <ul>
-                <?php foreach ($hotels as $hotel) : ?>
-                    <li>
-                        <h2>Name: <?= $hotel['name']; ?></h2>
-                        <p>Description: <?= $hotel['description']; ?></p>
-                        <p>Parking: <?= $hotel['parking'] ? 'Available' : 'Not available'; ?></p>
-                        <p>Vote: <?= $hotel['vote']; ?></p>
-                        <p>Distance to centre: <?= $hotel['distance_to_center']; ?> km</p>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+        <!-- Section -->
+        <section id="hotels" class="container">
+
+            <!-- H1 -->
+            <h1 class="my-5 text-center">Hotels </h1>
+
+            <!-- Table -->
+            <table class="table table-primary">
+
+                <!-- Thead -->
+                <thead>
+                    <tr>
+                        <th class="text-center">Name</th>
+                        <th class="text-center">Description</th>
+                        <th class="text-center">Parking</th>
+                        <th class="text-center">Vote</th>
+                        <th class="text-center">Distance to Centre</th>
+                    </tr>
+                </thead>
+
+                <!-- Tbody -->
+                <tbody>
+                    <!-- Foreach -->
+                    <?php foreach ($hotels as $hotel) : ?>
+                        <tr>
+                            <td class="text-center"><?= $hotel['name']; ?></td>
+                            <td class="text-center"><?= $hotel['description']; ?></td>
+                            <td class="text-center"><?= $hotel['parking'] ? 'Available' : 'Not available'; ?></td>
+                            <td class="text-center"><?= $hotel['vote']; ?></td>
+                            <td class="text-center"><?= $hotel['distance_to_center']; ?> km</td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         </section>
     </main>
 </body>
